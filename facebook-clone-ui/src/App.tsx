20a1d1e9a6554bd2f879a6988852560dc75a4a80
Thenia <theniakoutrouli@gmail.com>
2022-08-06 14:11:56 +0200

    created header

--- facebook-clone-ui/src/App.tsx
+++ facebook-clone-ui/src/App.tsx
@@ -1,10 +1,9 @@
 import './App.css';
+import {Header} from './Header'
 
 function App() {
   return (
-    <div className="App">
-      <div>Hello World!</div>
-    </div>
+    <Header/>
   );
 }
 
---
+++ facebook-clone-ui/src/Header.tsx
@@ -0,0 +1,8 @@
+
+export function Header() {
+    return (
+      <div className="App">
+        <div>Hello World!</div>
+      </div>
+    );
+}
