6d6a141a0dbb4011eb929251d8eef3a4586873f5
Thenia <theniakoutrouli@gmail.com>
2022-08-06 15:42:47 +0200

    made @material-ui work

--- facebook-clone-ui/src/Header/Header.tsx
+++ facebook-clone-ui/src/Header/Header.tsx
@@ -1,7 +1,15 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles(() =>({
+  header:{
+    fontSize: "30px"
+  }
+}))
 
 export function Header() {
+  const classes = useStyles();
     return (
-      <div className="App">
+      <div className={classes.header}>
         <div>Hello World!</div>
       </div>
     );
